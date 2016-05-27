8586ac0a5f57f111447c13a87c0554a53d0ffb49
Chris Simpson <c.a.simpson01@gmail.com>
2016-05-27 11:48:32 +0100

    Attempt to build travis with opencv.

--- lightct/tomo_scan.py
+++ lightct/tomo_scan.py
@@ -34,7 +34,7 @@ class TomoScan(LoadProjections):
         self.folder = folder
         self.p0 = 0
         self.cor_offset = 0
-        self.crop = None, None, None
+        self.crop = None, None, None, None
         self.num_images = None
         self.angles = None
         self.recon_data = None
@@ -44,17 +44,15 @@ class TomoScan(LoadProjections):
         camera = cv2.VideoCapture(camera_port)
         camera.set(3, 2000)
         camera.set(4, 2000)
-        _, im = camera.read()
         try:
-            dims = im[:, :, 2].shape + (num_proj, )
+            dims = camera.read()[1][:, :, 2].shape + (num_proj, )
         except TypeError:
             error = ('Camera returning None. Check camera settings (port) and'
                      ' ensure camera is not being run by other software.')
             raise TypeError(error)
         self.im_stack = np.zeros(dims)
 
-        print('\n\n')
-        # Acquires defined number of images (saves v from hsv)
+        # Acquires defined number of images (saves slice from hsv)
         for i in range(num_proj):
             _, im = camera.read()
             self.im_stack[:, :, i] = color.rgb2hsv(im)[:, :, hsv_dict[hsv]]
@@ -65,8 +63,7 @@ class TomoScan(LoadProjections):
             time.sleep(wait)
         del camera
 
-        self.height = self.im_stack.shape[0]
-        self.width = self.im_stack.shape[1]
+        self.height, self.width = self.im_stack.shape[:2]
 
         if save:
             save_folder = os.path.join(self.folder, 'projections')
