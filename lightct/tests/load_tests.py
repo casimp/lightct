f982dbf341b988c65d7a2b47668ae2bdc09a377c
Chris Simpson <c.a.simpson01@gmail.com>
2016-05-27 09:28:57 +0100

    Using mock to cover plotting functions.

--- lightct/tests/load_tests.py
+++ lightct/tests/load_tests.py
@@ -8,6 +8,7 @@ Created on Thu May 26 22:25:14 2016
 from nose.tools import assert_equal
 from lightct.load_scan import LoadProjections
 import os
+from mock import patch
 
 
 proj_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), r'test_scan/projections')
@@ -20,36 +21,49 @@ def test_load():
     num_tifs = len([i for i in os.listdir(proj_path) if i[-4:] == '.tif'])
     assert_equal(scan_data.im_stack.shape[-1], num_tifs)
 
-def test_auto_angles():
+
+@patch("matplotlib.pyplot.show")
+def test_auto_angles(mock_show):
     scan_data = LoadProjections(proj_path)
-    scan_data.auto_set_angles(20, plot=False)
+    scan_data.auto_set_angles(20)
     
+    
+@patch("matplotlib.pyplot.show")
+def test_manual_angles(mock_show):
+    scan_data = LoadProjections(proj_path)
+    scan_data.manual_set_angles()
+    
+
 def test_set_angles():
     scan_data = LoadProjections(proj_path)
     scan_data.set_angles(21, 360)
-
     
-def test_auto_centre():
+    
+@patch("matplotlib.pyplot.show")
+def test_auto_centre(mock_show):
     scan_data = LoadProjections(proj_path)
     scan_data.set_angles(21, 360)
-    scan_data.auto_centre(plot=False)
+    scan_data.auto_centre()
     
 def test_set_centre():
     scan_data = LoadProjections(proj_path)
     scan_data.set_angles(21, 360)
     scan_data.set_centre(-40)
     assert_equal(scan_data.cor_offset, -40)
-    
-def test_set_crop():
+
+
+@patch("matplotlib.pyplot.show")
+def test_set_crop(mock_show):
     scan_data = LoadProjections(proj_path)
     scan_data.set_angles(21, 360)
     scan_data.set_centre(-40)
     
     assert_equal(scan_data.crop, (None, None, None, None))
-    scan_data.set_crop(100, 100, 100, plot=False)
+    scan_data.set_crop(100, 100, 100)
     assert_equal(scan_data.crop, (100, -100, 100, -100))
     scan_data.set_crop(0, 0, 0, plot=False)
     assert_equal(scan_data.crop, (None, None, None, None))
+    
     
 def test_reconstruction():
     scan_data = LoadProjections(proj_path)
